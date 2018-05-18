8e4451ef81d716edd18c9aedaa320a7d37afdc8f
odax <nr10@albion.edu>
2018-05-18 14:02:36 -0400

    completed MVP
    -completed all steps
    -could not figure out how to access the jokes with .get, confused about postman

--- api/models/userModels.js
+++ api/models/userModels.js
@@ -4,24 +4,51 @@ const Schema = mongoose.Schema;
 
 const SALT_ROUNDS = 11;
 
-const UserSchema = Schema({
   // create your user schema here.
   // username: required, unique and lowercase
   // password: required
+
+  const UserSchema = Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  }
 });
 
-UserSchema.pre('save', function(next) {
   // https://github.com/kelektiv/node.bcrypt.js#usage
   // Fill this middleware in with the Proper password encrypting, bcrypt.hash()
   // if there is an error here you'll need to handle it by calling next(err);
   // Once the password is encrypted, call next() so that your userController and create a user
+  
+  UserSchema.pre('save', function(next) {
+  return bcrypt
+    .hash(this.password, 10)
+    .then(hash => {
+      this.password = hash;
+      return next();
+    })
+    .catch(err => {
+      return next(err);
+    });
 });
 
-UserSchema.methods.checkPassword = function(plainTextPW, callBack) {
-  // https://github.com/kelektiv/node.bcrypt.js#usage
+// https://github.com/kelektiv/node.bcrypt.js#usage
   // Fill this method in with the Proper password comparing, bcrypt.compare()
   // Your controller will be responsible for sending the information here for password comparison
   // Once you have the user, you'll need to pass the encrypted pw and the plaintext pw to the compare function
+
+UserSchema.methods.checkPassword = function(plainTextPW, callBack) {
+  bcrypt.compare(plainTextPW, this.password, function(err, isValid) {
+    if (err) return callBack(err);
+
+    callBack(null, isValid);
+})
 };
 
 module.exports = mongoose.model('User', UserSchema);
